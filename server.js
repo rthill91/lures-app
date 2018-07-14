3b34688b1e303edd43fd8e03d774c166b890866f
Ryan Hill <rthill91@gmail.com>
2018-07-14 17:58:34 -0500

    test api

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/api/test', (req, res) => {
+  res.json('asdf');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
